eb31a88d93d122d182c18d1ccc8458a514ce0469
karth <karthick.vasudevan07@gmail.com>
2024-07-16 21:23:40 +0530

    video Thumbnail issue fixed

--- frontend/youtube-clone-ui/src/app/components/thumbnail-video-player/thumbnail-video-player.component.ts
+++ frontend/youtube-clone-ui/src/app/components/thumbnail-video-player/thumbnail-video-player.component.ts
@@ -9,4 +9,7 @@ export class ThumbnailVideoPlayerComponent {
   @Input()
   url =
     'https://youtube-clone-avk.s3.ap-south-1.amazonaws.com/2adfcab6-dc28-47eb-af60-2e200ff5ce70.mp4';
+
+  @Input()
+  thumbnailImageUrl = '';
 }
